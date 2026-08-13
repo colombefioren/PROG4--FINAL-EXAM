b35931610f404b586888b003b3fb09501cc25816
colombefioren <colomberakotonjanahary@gmail.com>
2026-08-13 20:01:36 +0300

    feat: add grade request and response dto

---
+++ src/main/java/org/cocojojo/mg/endpoint/rest/controller/dto/GradeRequest.java
@@ -0,0 +1,15 @@
+package org.cocojojo.mg.endpoint.rest.controller.dto;
+
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.util.UUID;
+import lombok.Builder;
+
+@Builder
+public record GradeRequest(
+    @NotNull UUID studentId,
+    @NotNull UUID examId,
+    @NotNull @DecimalMin("0") @DecimalMax("20") BigDecimal value,
+    String comment) {}
