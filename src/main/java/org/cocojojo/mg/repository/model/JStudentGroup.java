d715abf080b9410c72250bfd9fd0bad3a737264f
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-08-13 12:35:58 +0300

    feat: JStudentGroup and Track enum (EL, TN)

---
+++ src/main/java/org/cocojojo/mg/repository/model/JStudentGroup.java
@@ -0,0 +1,45 @@
+package org.cocojojo.mg.repository.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.UUID;
+
+import org.cocojojo.mg.model.enums.Track;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+impTrack mg.hei.notes.model.enums.Track;
+
+@Entity
+@Table(name = "student_group")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class JStudentGroup {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @ManyToOne
+  @JoinColumn(name = "promotion_id", nullable = false)
+  private JPromotion promotion;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Enumerated(EnumType.STRING)
+  private Track track;
+}
