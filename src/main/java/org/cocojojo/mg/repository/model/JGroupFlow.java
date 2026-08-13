83a699687d523535cbd689b657816e7fa83088b1
colombefioren <colomberakotonjanahary@gmail.com>
2026-08-13 14:16:31 +0300

    feat: enhance group tracking with join and leave events

--- src/main/java/org/cocojojo/mg/repository/model/JGroupFlow.java
+++ src/main/java/org/cocojojo/mg/repository/model/JGroupFlow.java
@@ -1,13 +1,9 @@
 package org.cocojojo.mg.repository.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import static jakarta.persistence.EnumType.STRING;
+import static org.hibernate.type.SqlTypes.NAMED_ENUM;
+
+import jakarta.persistence.*;
 import java.time.Instant;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
@@ -15,6 +11,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.cocojojo.mg.model.enums.GroupFlowType;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
 
 @Entity
 @Table(name = "group_flow")
@@ -36,6 +35,12 @@ public class JGroupFlow {
   @JoinColumn(name = "group_id", nullable = false)
   private JGroup group;
 
-  @Column(name = "effective_datetime", nullable = false)
-  private Instant effectiveDatetime;
+  @Column(name = "group_flow_type")
+  @Enumerated(STRING)
+  @JdbcTypeCode(NAMED_ENUM)
+  private GroupFlowType groupFlowType;
+
+  @CreationTimestamp
+  @Column(name = "flow_datetime", nullable = false)
+  private Instant flowDatetime;
 }
