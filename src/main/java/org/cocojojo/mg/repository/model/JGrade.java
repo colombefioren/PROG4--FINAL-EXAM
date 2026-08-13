a0ebb4847621cd24a383e395bd3c8cf58d97ea8c
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-08-13 17:46:45 +0300

    fix: add missing UniqueConstraint import

--- src/main/java/org/cocojojo/mg/repository/model/JGrade.java
+++ src/main/java/org/cocojojo/mg/repository/model/JGrade.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.UUID;
