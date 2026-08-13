af823dd85793e09a0a870b2fd1c4d00984fefd8f
colombefioren <colomberakotonjanahary@gmail.com>
2026-08-13 20:02:17 +0300

    feat: add student request and response dto

---
+++ src/main/java/org/cocojojo/mg/endpoint/rest/controller/dto/StudentResponse.java
@@ -0,0 +1,14 @@
+package org.cocojojo.mg.endpoint.rest.controller.dto;
+
+import java.util.UUID;
+import lombok.Builder;
+
+@Builder
+public record StudentResponse(
+    UUID id,
+    String std,
+    String firstname,
+    String lastname,
+    String email,
+    UUID promotionId,
+    String promotionName) {}
