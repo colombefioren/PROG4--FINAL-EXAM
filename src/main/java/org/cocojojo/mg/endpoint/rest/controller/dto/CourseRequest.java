ca487054c9c65ad997f122a65827a494845aafdf
colombefioren <colomberakotonjanahary@gmail.com>
2026-08-13 19:59:58 +0300

    feat: fix course request validation and add course response

---
+++ src/main/java/org/cocojojo/mg/endpoint/rest/controller/dto/CourseRequest.java
@@ -0,0 +1,20 @@
+package org.cocojojo.mg.endpoint.rest.controller.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import java.util.UUID;
+import lombok.Builder;
+import org.cocojojo.mg.model.enums.StudentLevel;
+import org.cocojojo.mg.model.enums.Track;
+
+@Builder
+public record CourseRequest(
+    UUID id,
+    @NotBlank String code,
+    @NotBlank String name,
+    @NotNull @Min(1) Integer credits,
+    @Positive Integer totalHours,
+    @NotNull StudentLevel studentLevel,
+    @NotNull Track track) {}
