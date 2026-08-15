6ee7088fe63d4806b8b55fcace7f439729a9bf15
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-08-15 15:42:05 +0300

    feat: add group flow repository

---
+++ src/main/java/org/cocojojo/mg/repository/GroupFlowRepository.java
@@ -0,0 +1,9 @@
+package org.cocojojo.mg.repository;
+
+import java.util.UUID;
+import org.cocojojo.mg.repository.model.JGroupFlow;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroupFlowRepository extends JpaRepository<JGroupFlow, UUID> {}
