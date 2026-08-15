21974a22b2984a932ec063b13a6cf15c7ae34c7c
colombefioren <colomberakotonjanahary@gmail.com>
2026-08-15 22:58:17 +0300

    feat: add grade repository queries

--- src/main/java/org/cocojojo/mg/repository/GradeHistoryRepository.java
+++ src/main/java/org/cocojojo/mg/repository/GradeHistoryRepository.java
@@ -1,9 +1,13 @@
 package org.cocojojo.mg.repository;
 
+import java.util.List;
 import java.util.UUID;
 import org.cocojojo.mg.repository.model.JGradeHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GradeHistoryRepository extends JpaRepository<JGradeHistory, UUID> {}
+public interface GradeHistoryRepository extends JpaRepository<JGradeHistory, UUID> {
+
+  List<JGradeHistory> findByGradeIdOrderByChangedAtDesc(UUID gradeId);
+}
--- src/main/java/org/cocojojo/mg/repository/GradeRepository.java
+++ src/main/java/org/cocojojo/mg/repository/GradeRepository.java
@@ -1,9 +1,40 @@
 package org.cocojojo.mg.repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.cocojojo.mg.repository.model.JGrade;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GradeRepository extends JpaRepository<JGrade, UUID> {}
+public interface GradeRepository extends JpaRepository<JGrade, UUID> {
+
+  @EntityGraph(
+      attributePaths = {
+        "exam",
+        "exam.courseAssignment",
+        "exam.courseAssignment.teachers",
+        "student"
+      })
+  List<JGrade> findByExamId(UUID examId);
+
+  @EntityGraph(
+      attributePaths = {
+        "exam",
+        "exam.courseAssignment",
+        "exam.courseAssignment.teachers",
+        "student"
+      })
+  List<JGrade> findByStudentId(UUID studentId);
+
+  @EntityGraph(
+      attributePaths = {
+        "exam",
+        "exam.courseAssignment",
+        "exam.courseAssignment.teachers",
+        "student"
+      })
+  Optional<JGrade> findByExamIdAndStudentId(UUID examId, UUID studentId);
+}
