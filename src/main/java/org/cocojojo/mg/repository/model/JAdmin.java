61f037ca3f59420f16073453dd56231d2266ded9
colombefioren <colomberakotonjanahary@gmail.com>
2026-08-15 18:16:05 +0300

    refactor: add superbuilder to admin and teacher entities

--- src/main/java/org/cocojojo/mg/repository/model/JAdmin.java
+++ src/main/java/org/cocojojo/mg/repository/model/JAdmin.java
@@ -3,9 +3,11 @@ package org.cocojojo.mg.repository.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Table(name = "\"admin\"")
@@ -13,4 +15,6 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
 public class JAdmin extends JUser {}
--- src/main/java/org/cocojojo/mg/repository/model/JTeacher.java
+++ src/main/java/org/cocojojo/mg/repository/model/JTeacher.java
@@ -3,14 +3,18 @@ package org.cocojojo.mg.repository.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Table(name = "\"teacher\"")
 @PrimaryKeyJoinColumn(name = "\"id\"")
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
+@SuperBuilder
 public class JTeacher extends JUser {}
