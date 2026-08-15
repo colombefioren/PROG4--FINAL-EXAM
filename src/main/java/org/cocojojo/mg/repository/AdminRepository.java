6f5614d56b491eb41a4b8080edcb8441c3dc68ab
colombefioren <colomberakotonjanahary@gmail.com>
2026-08-15 17:21:31 +0300

    refactor: remove unused find by email from role repositories

--- src/main/java/org/cocojojo/mg/repository/AdminRepository.java
+++ src/main/java/org/cocojojo/mg/repository/AdminRepository.java
@@ -1,12 +1,9 @@
 package org.cocojojo.mg.repository;
 
-import java.util.Optional;
 import java.util.UUID;
 import org.cocojojo.mg.repository.model.JAdmin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdminRepository extends JpaRepository<JAdmin, UUID> {
-  Optional<JAdmin> findByEmailIgnoreCase(String email);
-}
+public interface AdminRepository extends JpaRepository<JAdmin, UUID> {}
