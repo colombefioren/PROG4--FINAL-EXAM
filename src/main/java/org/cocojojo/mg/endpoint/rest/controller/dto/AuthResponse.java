5d1a42a5bf884b96141f2a74e5354815d6128a9d
colombefioren <colomberakotonjanahary@gmail.com>
2026-08-15 16:33:42 +0300

    refactor: nest user response inside auth response

--- src/main/java/org/cocojojo/mg/endpoint/rest/controller/dto/AuthResponse.java
+++ src/main/java/org/cocojojo/mg/endpoint/rest/controller/dto/AuthResponse.java
@@ -1,8 +1,6 @@
 package org.cocojojo.mg.endpoint.rest.controller.dto;
 
 import lombok.Builder;
-import org.cocojojo.mg.model.enums.Role;
 
 @Builder
-public record AuthResponse(
-    String token, String userId, Role role, String firstName, String lastName) {}
+public record AuthResponse(String token, UserResponse user) {}
