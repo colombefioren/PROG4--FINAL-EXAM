0b8aa05b906a941ff1cfa416832fa1e929ba3e4c
colombefioren <colomberakotonjanahary@gmail.com>
2026-08-15 22:16:03 +0300

    feat: add student current group flow query

--- src/main/java/org/cocojojo/mg/repository/GroupFlowRepository.java
+++ src/main/java/org/cocojojo/mg/repository/GroupFlowRepository.java
@@ -1,9 +1,13 @@
 package org.cocojojo.mg.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 import org.cocojojo.mg.repository.model.JGroupFlow;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GroupFlowRepository extends JpaRepository<JGroupFlow, UUID> {}
+public interface GroupFlowRepository extends JpaRepository<JGroupFlow, UUID> {
+
+  Optional<JGroupFlow> findFirstByStudentIdOrderByCreatedAtDesc(UUID studentId);
+}
