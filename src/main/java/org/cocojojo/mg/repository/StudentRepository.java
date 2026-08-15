a13edc8dd942311ab149f3853b271793dd6c2511
colombefioren <colomberakotonjanahary@gmail.com>
2026-08-15 16:08:35 +0300

    feat: add find by email to account repositories

--- src/main/java/org/cocojojo/mg/repository/StudentRepository.java
+++ src/main/java/org/cocojojo/mg/repository/StudentRepository.java
@@ -1,9 +1,12 @@
 package org.cocojojo.mg.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 import org.cocojojo.mg.repository.model.JStudent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository extends JpaRepository<JStudent, UUID> {}
+public interface StudentRepository extends JpaRepository<JStudent, UUID> {
+  Optional<JStudent> findByEmailIgnoreCase(String email);
+}
