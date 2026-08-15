642b464d2000f03ccd7e803f7c86b2cb162339a7
colombefioren <colomberakotonjanahary@gmail.com>
2026-08-15 18:15:21 +0300

    feat: enable http basic authentication

--- src/main/java/org/cocojojo/mg/endpoint/rest/security/SecurityConfig.java
+++ src/main/java/org/cocojojo/mg/endpoint/rest/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.cocojojo.mg.endpoint.rest.security;
 
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.PUT;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -63,6 +64,7 @@ public class SecurityConfig {
                 ex.authenticationEntryPoint(
                     (request, response, authException) ->
                         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")))
+        .httpBasic(withDefaults())
         .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
         .build();
   }
