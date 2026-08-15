91d5ef2006bea5f12ff4a36650656f0d73ec93ae
colombefioren <colomberakotonjanahary@gmail.com>
2026-08-15 19:12:50 +0300

    feat: add std reference generator

---
+++ src/main/java/org/cocojojo/mg/util/StdRefGenerator.java
@@ -0,0 +1,24 @@
+package org.cocojojo.mg.util;
+
+import lombok.RequiredArgsConstructor;
+import org.cocojojo.mg.repository.StudentRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class StdRefGenerator {
+
+  private final StudentRepository studentRepository;
+
+  public String generate(int entryYear) {
+    var prefix = "STD" + String.format("%02d", entryYear % 100);
+    return studentRepository
+        .findFirstByStdStartingWithOrderByStdDesc(prefix)
+        .map(last -> prefix + String.format("%03d", nextSequence(last.getStd(), prefix)))
+        .orElse(prefix + "001");
+  }
+
+  private int nextSequence(String lastStd, String prefix) {
+    return Integer.parseInt(lastStd.substring(prefix.length())) + 1;
+  }
+}
