ccad952aad24950a0e875a9dce991d512540089b
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-08-15 18:10:41 +0300

    fix: drop course_assignment constraint before its column

--- src/main/resources/db/migration/V58__Create_course_assignment_teacher_table.sql
+++ src/main/resources/db/migration/V58__Create_course_assignment_teacher_table.sql
@@ -10,8 +10,7 @@ select id, teacher_id
 from course_assignment
 where teacher_id is not null;
 
-alter table course_assignment drop column teacher_id;
-
 alter table course_assignment drop constraint course_assignment_uk;
+alter table course_assignment drop column teacher_id;
 alter table course_assignment
     add constraint course_assignment_uk unique (course_id, group_id, academic_year, semester);
