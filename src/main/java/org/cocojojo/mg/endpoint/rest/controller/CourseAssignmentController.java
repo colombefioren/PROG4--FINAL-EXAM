e0b585f8ebb0975d74a6bce2800c241d52eb1c68
colombefioren <colomberakotonjanahary@gmail.com>
2026-08-15 20:33:01 +0300

    fix: return 204 on course assignment deletion

--- src/main/java/org/cocojojo/mg/endpoint/rest/controller/CourseAssignmentController.java
+++ src/main/java/org/cocojojo/mg/endpoint/rest/controller/CourseAssignmentController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.cocojojo.mg.endpoint.rest.controller.dto.CourseAssignmentRequest;
 import org.cocojojo.mg.endpoint.rest.controller.dto.CourseAssignmentResponse;
 import org.cocojojo.mg.service.CourseAssignmentService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -45,6 +47,7 @@ public class CourseAssignmentController {
   }
 
   @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
   public void delete(@PathVariable UUID id) {
     service.delete(id);
   }
