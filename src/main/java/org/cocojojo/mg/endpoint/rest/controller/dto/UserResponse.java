9db5e86d67628edefcd865b6f8b6a205dd950217
colombefioren <colomberakotonjanahary@gmail.com>
2026-08-15 09:37:28 +0300

    feat: add role to user response

--- src/main/java/org/cocojojo/mg/endpoint/rest/controller/dto/UserResponse.java
+++ src/main/java/org/cocojojo/mg/endpoint/rest/controller/dto/UserResponse.java
@@ -2,6 +2,7 @@ package org.cocojojo.mg.endpoint.rest.controller.dto;
 
 import java.util.UUID;
 import lombok.Builder;
+import org.cocojojo.mg.model.enums.Role;
 
 @Builder
-public record UserResponse(UUID id, String firstname, String lastname, String email) {}
+public record UserResponse(UUID id, String firstname, String lastname, String email, Role role) {}
