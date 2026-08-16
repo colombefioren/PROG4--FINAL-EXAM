78f98cae2b22cf8e178669034477505127643f65
colombefioren <colomberakotonjanahary@gmail.com>
2026-08-16 13:08:06 +0300

    style: reorder imports in graduates it

--- src/test/java/org/cocojojo/mg/it/GraduatesIT.java
+++ src/test/java/org/cocojojo/mg/it/GraduatesIT.java
@@ -50,8 +50,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
