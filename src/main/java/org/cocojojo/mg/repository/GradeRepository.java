8f6ef5808224bf08a256d6c5f26f88e316073cb5
colombefioren <colomberakotonjanahary@gmail.com>
2026-08-16 20:36:05 +0300

    chore: format code

--- src/main/java/org/cocojojo/mg/repository/GradeRepository.java
+++ src/main/java/org/cocojojo/mg/repository/GradeRepository.java
@@ -3,12 +3,9 @@ package org.cocojojo.mg.repository;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import org.cocojojo.mg.model.enums.Semester;
 import org.cocojojo.mg.repository.model.JGrade;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
