e0bb032d985db0fcea9cdeb26a2b8a43c2e6a5e0
colombefioren <colomberakotonjanahary@gmail.com>
2026-08-16 07:59:18 +0300

    refactor: derive student curriculum from group assignments

--- src/main/java/org/cocojojo/mg/repository/CourseAssignmentRepository.java
+++ src/main/java/org/cocojojo/mg/repository/CourseAssignmentRepository.java
@@ -1,8 +1,10 @@
 package org.cocojojo.mg.repository;
 
+import java.util.List;
 import java.util.UUID;
 import org.cocojojo.mg.model.enums.Semester;
 import org.cocojojo.mg.repository.model.JCourseAssignment;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,7 @@ public interface CourseAssignmentRepository extends JpaRepository<JCourseAssignm
 
   boolean existsByCourseIdAndGroupIdAndAcademicYearAndSemester(
       UUID courseId, UUID groupId, int academicYear, Semester semester);
+
+  @EntityGraph(attributePaths = {"course"})
+  List<JCourseAssignment> findByGroupIdIn(List<UUID> groupIds);
 }
