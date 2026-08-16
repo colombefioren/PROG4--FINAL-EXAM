fd9e0474d6e24720761de086521282de09e8ac92
colombefioren <colomberakotonjanahary@gmail.com>
2026-08-16 10:16:23 +0300

    refactor: drop vestigial /view/** matcher

--- src/main/java/org/cocojojo/mg/endpoint/rest/security/SecurityConfig.java
+++ src/main/java/org/cocojojo/mg/endpoint/rest/security/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
                         "/swagger-ui.html",
                         "/swagger-ui/**")
                     .permitAll()
-                    .requestMatchers("/view/**", "/ui/**")
+                    .requestMatchers("/ui/**")
                     .hasRole("ADMIN")
                     .requestMatchers(
                         PUT,
