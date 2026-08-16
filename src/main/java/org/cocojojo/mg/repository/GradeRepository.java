c41742a29dfe34503f28b1736eacdc00dd688fcd
colombefioren <colomberakotonjanahary@gmail.com>
2026-08-16 07:55:50 +0300

    feat: add graduate computation repository queries

--- src/main/java/org/cocojojo/mg/repository/GradeRepository.java
+++ src/main/java/org/cocojojo/mg/repository/GradeRepository.java
@@ -1,9 +1,21 @@
 package org.cocojojo.mg.repository;
 
+import java.util.List;
 import java.util.UUID;
 import org.cocojojo.mg.repository.model.JGrade;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GradeRepository extends JpaRepository<JGrade, UUID> {}
+public interface GradeRepository extends JpaRepository<JGrade, UUID> {
+
+  @EntityGraph(
+      attributePaths = {
+        "exam",
+        "exam.courseAssignment",
+        "exam.courseAssignment.course",
+        "exam.courseAssignment.teachers"
+      })
+  List<JGrade> findByStudentId(UUID studentId);
+}
--- src/main/java/org/cocojojo/mg/repository/GroupFlowRepository.java
+++ src/main/java/org/cocojojo/mg/repository/GroupFlowRepository.java
@@ -1,9 +1,13 @@
 package org.cocojojo.mg.repository;
 
+import java.util.List;
 import java.util.UUID;
 import org.cocojojo.mg.repository.model.JGroupFlow;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GroupFlowRepository extends JpaRepository<JGroupFlow, UUID> {}
+public interface GroupFlowRepository extends JpaRepository<JGroupFlow, UUID> {
+
+  List<JGroupFlow> findByStudentId(UUID studentId);
+}
